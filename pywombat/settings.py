30f36b68b4a4eee1efc097b2409560138c74767a
Eduardo <eduardo78d@gmail.com>
2020-12-02 17:16:43 -0600

    archivo requirements.txt

--- pywombat/settings.py
+++ pywombat/settings.py
@@ -72,14 +72,12 @@ WSGI_APPLICATION = 'pywombat.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -112,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
